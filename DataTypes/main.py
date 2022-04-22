29a08133545a2a3ec4f3dcdb89d9326fd418a34f
Sahithi Bondili <104204208+Sahithi-Bondili@users.noreply.github.com>
2022-04-22 19:11:36 +0530

    Create main.py

---
+++ DataTypes/main.py
@@ -0,0 +1,13 @@
+# String Concatination
+# f-string
+firstName = "sowmya"
+lastName = "bondili"
+age = 25
+grade = 9.8
+print(f"Hello {firstName}.{lastName} you are {age} years old. you got {grade} in your graduation")
+
+# String-Length calculation
+name = input("enter the name")
+length = len(name)
+print("it has" + " "  + str(length) + " " + "characters")
+print(f"it has {length} characters")
